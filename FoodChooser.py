8fa48f58ffba53c8bc7b64366de118dd4040dd01
Dean <deang467@gmail.com>
2021-11-25 09:19:45 -0500

    Putting foods into sets
    Issues: Cannot get name of food name. Fix soon

--- FoodChooser.py
+++ FoodChooser.py
@@ -15,7 +15,7 @@ class Food:
     """
     
     def __init__(self, macronutrient: str, calories=0, 
-                 carbs=0, fats=0, proteins=0) -> None:
+                 carbs=0, fats=0, protein=0) -> None:
         """
         Initialize and declare each attribute
         
@@ -34,7 +34,10 @@ class Food:
         self._calories = calories
         self._carbs = carbs
         self._fats = fats
-        self._proteins = proteins
+        self._protein = protein
+        
+    def getMacronutrient(self) -> str:
+        return self._macronutrient
     
     def getCalories(self) -> int:
         return self._calories
@@ -49,24 +52,58 @@ class Food:
         return self._proteins
 
 
-if __name__ == '__main__':
+# ------------Functions below make the main function cleaner-------------------
+def startProgram() -> None:
     print("Instructions:")
     print("After entering an ingredient, click enter. " +
           "Then enter the next one")
-    print("When you are done, please type finished")
+    print("When you are done, please type done")
     print("What ingredients do you have?")
+
+def loopJSON() -> None:
+    # For every food in data
+    for detail in data['details']:
+        
+        #Only create object if food is part of the ingredients
+        if detail['food'] in ingredients:
+            detail['food'] = Food(detail['macronutrient'], 
+                                           detail['calories'], detail['carbs'], 
+                                           detail['fats'], detail['protein'])
+            # Add to sets
+            if detail['food'].getMacronutrient() == 'carb':
+                carbs.add(detail['food'])
+            elif detail['food'].getMacronutrient() == 'fat':
+                fats.add(detail['food'])
+            else:
+                proteins.add(detail['food'])
+
+#------------------------------End Of Functions--------------------------------
+
+if __name__ == '__main__':
+    
+    startProgram()
     ingredients = []
-    finished = False
-    while not finished:
+    done = False
+    while not done:
         food = input("")
-        if food == "finished":
-            finished = True
+        if food == "done":
+            done = True
         else:
             ingredients.append(food.strip())
+
     f = open('data.json')
     data = json.load(f)
-    for i in data['details']:
-        print(i)
+    
+    # Sets
+    carbs = set([])
+    fats = set([])
+    proteins = set([])
+    
+    loopJSON()
+        
+    print(carbs)
+    print(fats)
+    print(proteins)
     f.close()
     
     
